7d22e453413ea05746c1568f27d1d06c2353dbdd
Daniel Javorszky <daniel.javorszky@liferay.com>
2018-06-09 15:44:30 +0200

    Visibility genesis

--- dependency/dependency.go
+++ dependency/dependency.go
@@ -42,36 +42,69 @@ func NewWithCycles(allowCycles bool) *Dependency {
 	return &dependency
 }
 
-// Add adds a new dependency to the dependent, as well
-// as sets the corresponding visibility if needed.
-func (d *Dependency) Add(dependent, dependee string) error {
-	if dependent == "" || dependee == "" {
+// Add adds a new dependency to the depender, as well as set the
+// corresponding visibility.
+//
+// If A depends on B, then A is the depender, and B is the dependent.
+// In the above scenario, B needs to be visible to A.
+func (d *Dependency) Add(depender, dependent string) error {
+	if depender == "" || dependent == "" {
 		return fmt.Errorf("empty dependant or dependee")
 	}
 
 	d.depRW.Lock()
-	d.mustAddDependency(dependent, dependee)
+	d.mustAddDependency(depender, dependent)
 	d.depRW.Unlock()
+
+	d.visRW.Lock()
+	d.mustAddVisibility(dependent, depender)
+	d.visRW.Unlock()
 
 	return nil
 }
 
 // mustAddDependency is not concurrent-safe.
-func (d *Dependency) mustAddDependency(dependent, dependee string) {
-	if dependent == "" || dependee == "" {
+//
+// If A depends on B, then A is the depender, and B is the dependent.
+func (d *Dependency) mustAddDependency(depender, dependent string) {
+	if depender == "" || dependent == "" {
 		panic("empty dependent or dependee")
 	}
 
-	dependees := d.deps[dependent]
+	dependees := d.deps[depender]
 
 	// Check if dependency already exists
 	for _, dep := range dependees {
-		if dependee == dep {
+		if dependent == dep {
 			return
 		}
 	}
 
-	dependees = append(dependees, dependee)
+	dependees = append(dependees, dependent)
 
-	d.deps[dependent] = dependees
+	d.deps[depender] = dependees
+}
+
+// mustAddVisibility is not concurrent-safe.
+//
+// If A depends on B, then A is the depender, B is the dependent.
+// In this scenario, B needs to be made visible to A, so we can say
+// that B is being stalked by A, who is the stalker.
+func (d *Dependency) mustAddVisibility(stalked, stalker string) {
+	if stalked == "" || stalker == "" {
+		panic("empty stalked or stalker")
+	}
+
+	stalkers := d.visibilities[stalked]
+
+	// Check if stalking already exists
+	for _, s := range stalkers {
+		if stalker == s {
+			return
+		}
+	}
+
+	stalkers = append(stalkers, stalker)
+
+	d.visibilities[stalked] = stalkers
 }
--- dependency/dependency_test.go
+++ dependency/dependency_test.go
@@ -6,38 +6,38 @@ import (
 
 func TestDependency_doAddDependency(t *testing.T) {
 	type args struct {
+		depender  string
 		dependent string
-		dependee  string
 	}
 	tests := []struct {
 		name string
 		args []args
 	}{
-		{"First", []args{args{"First", "dependee"}}},
-		{"Two runs", []args{args{"Two runs", "dependee1"}, args{"Two runs", "dependee2"}}},
-		{"Three runs", []args{args{"Three runs", "dependee1"}, args{"Three runs", "dependee2"}, args{"Three runs", "dependee3"}}},
+		{"First", []args{args{"First", "depender"}}},
+		{"Two runs", []args{args{"Two runs", "depender1"}, args{"Two runs", "depender2"}}},
+		{"Three runs", []args{args{"Three runs", "depender1"}, args{"Three runs", "depender2"}, args{"Three runs", "depender3"}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			d := New()
 			for _, depTest := range tt.args {
-				d.mustAddDependency(depTest.dependent, depTest.dependee)
+				d.mustAddDependency(depTest.depender, depTest.dependent)
 
-				res, ok := d.deps[depTest.dependent]
+				res, ok := d.deps[depTest.depender]
 				if !ok {
-					t.Errorf("failed to add dependency: %s -> %s", depTest.dependent, depTest.dependee)
+					t.Errorf("failed to add dependency: %s -> %s", depTest.depender, depTest.dependent)
 				}
 
 				found := false
 				for _, d := range res {
-					if d == depTest.dependee {
+					if d == depTest.dependent {
 						found = true
 						break
 					}
 				}
 
 				if !found {
-					t.Errorf("didn't find added dependency: %s -> %s", depTest.dependent, depTest.dependee)
+					t.Errorf("didn't find added dependency: %s -> %s", depTest.depender, depTest.dependent)
 				}
 			}
 
@@ -51,8 +51,8 @@ func TestDependency_doAddDependency(t *testing.T) {
 
 func TestDependency_Add(t *testing.T) {
 	type args struct {
+		depender  string
 		dependent string
-		dependee  string
 	}
 	tests := []struct {
 		name    string
@@ -70,35 +70,72 @@ func TestDependency_Add(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			d := New()
 			for _, depTest := range tt.args {
-				if err := d.Add(depTest.dependent, depTest.dependee); (err != nil) != tt.wantErr {
+				if err := d.Add(depTest.depender, depTest.dependent); (err != nil) != tt.wantErr {
 					t.Errorf("Dependency.Add() error = %v, wantErr %v", err, tt.wantErr)
+					return
 				}
 
 				if tt.wantErr {
 					return
 				}
+			}
 
-				res, ok := d.deps[depTest.dependent]
+			// one depender, many dependents
+			depLen := len(d.deps[tt.name])
+			if len(tt.args) != depLen {
+				t.Errorf("length of resulting dependency list is not as expected. Expected: %d, actual: %d", len(tt.args), depLen)
+			}
+
+			// many stalked, only 1 stalker each
+			visLen := len(d.visibilities)
+			if len(tt.args) != visLen {
+				t.Errorf("length of resulting visibility list is not as expected. Expected: %d, actual: %d", len(tt.args), visLen)
+			}
+		})
+	}
+}
+
+func TestDependency_mustAddVisibility(t *testing.T) {
+	type args struct {
+		stalked string
+		stalker string
+	}
+	tests := []struct {
+		name string
+		args []args
+	}{
+		{"First", []args{args{"First", "stalker"}}},
+		{"Two runs", []args{args{"Two runs", "stalker1"}, args{"Two runs", "stalker2"}}},
+		{"Three runs", []args{args{"Three runs", "stalker1"}, args{"Three runs", "stalker2"}, args{"Three runs", "stalker3"}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := New()
+			for _, depTest := range tt.args {
+				d.mustAddVisibility(depTest.stalked, depTest.stalker)
+
+				res, ok := d.visibilities[depTest.stalked]
 				if !ok {
-					t.Errorf("failed to add dependency: %s -> %s", depTest.dependent, depTest.dependee)
+					t.Errorf("failed to add visibility: %s -> %s", depTest.stalked, depTest.stalker)
+					continue
 				}
 
 				found := false
 				for _, d := range res {
-					if d == depTest.dependee {
+					if d == depTest.stalker {
 						found = true
 						break
 					}
 				}
 
 				if !found {
-					t.Errorf("didn't find added dependency: %s -> %s", depTest.dependent, depTest.dependee)
+					t.Errorf("didn't find added visibility: %s -> %s", depTest.stalked, depTest.stalker)
 				}
 			}
 
-			actualLen := len(d.deps[tt.name])
-			if len(tt.args) != actualLen {
-				t.Errorf("length of resulting dependency list is not as expected. Expected: %d, actual: %d", len(tt.args), actualLen)
+			actualLen := len(d.visibilities[tt.name])
+			if actualLen != len(tt.args) {
+				t.Errorf("length of resulting visibility list is not as expected. Expected: %d, actual: %d", len(tt.args), actualLen)
 			}
 		})
 	}
