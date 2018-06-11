7f1ef4b33468635ebba087e1654586526023982d
Daniel Javorszky <daniel.javorszky@liferay.com>
2018-06-11 15:11:29 +0200

    Add option to read from file

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,49 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/djavorszky/depser"
 )
 
+var sources []string
+
 func main() {
 	if len(os.Args) == 1 {
-		fmt.Println("Please specify one or more paths to check")
+		fmt.Println("Please specify one or more paths to check, or a filename with -f")
 		os.Exit(1)
 	}
+	epoch := time.Now()
 
-	dep, err := depser.BuildDependencies(true, os.Args[1:])
+	fileName := flag.String("f", "none", "file to read for sources")
+
+	flag.Parse()
+
+	var err error
+	if *fileName == "none" {
+		sources = os.Args[1:]
+	} else {
+		sources, err = parseFile(*fileName)
+		if err != nil {
+			fmt.Printf("Failed parsing file: %v", err)
+			os.Exit(1)
+		}
+	}
+
+	start := time.Now()
+	fmt.Println("Building dependencies")
+	dep, err := depser.BuildDependencies(true, sources)
 	if err != nil {
 		fmt.Printf("failed building dependencies: %v\n", err)
 	}
+	fmt.Printf("Dependencies built in %s\n", time.Since(start))
 
+	start = time.Now()
+	fmt.Println("Checking cyclic dependencies")
 	cyclics, ok := dep.CheckCyclicDependencies()
 	if !ok {
 		fmt.Printf("%d dependency cycle(s) detected:\n", len(cyclics))
@@ -25,4 +51,27 @@ func main() {
 			fmt.Println(cycle)
 		}
 	}
+
+	fmt.Printf("Cyclic dependency check done in %s\n", time.Since(start))
+	fmt.Printf("Whole process took %s\n", time.Since(epoch))
+}
+
+func parseFile(fileName string) ([]string, error) {
+	var sources []string
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("Failed opening file: %v", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		sources = append(sources, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("reading from io.Reader: %v", err)
+	}
+
+	return sources, nil
 }
